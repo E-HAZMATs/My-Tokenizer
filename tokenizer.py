ac3482a49e9cb4f1b627bca14359c223de12ba13
eHAZMATs <alzamarkal@gmail.com>
2026-07-07 14:57:30 +0300

    Checkpoint.

--- tokenizer.py
+++ tokenizer.py
@@ -10,19 +10,50 @@ encode text -> add vocab _ replace encoded text by finding toeknized pairs and r
 
 - confused about vocab, I guess it should be a dict. Key = byte or byte pair (but key as byte pair would get so long tho? say one token is the result of merge 10 times, ti's key will be huge?)
 '''
+
+'''
+GENERAL TODOS:
+TODO: Handle equality of freq occurrence? maybe no need, since the other pair will get selected next iter?
+TODO: Try on Arabic dataset. Drop taylor swify txt.
+'''
 class Tokenizer:
     
     def __init__(self):
-        self.vocab = None
+
+        # The tokenizer should be able to generalize and encode tokens not in the training set.
+        # Each possible character can be represented into 1-4 bytes in UTF-8. So if ت for example is not in training set,
+        # then its utf8 representation, say for example it's [50, 200], will encode the letter as the 50 and 200 byte tokens.
+        # Of course this is would cause a problem cus the GPT model's embedding for those bytes would probably be less learnt.
+        # TODO: What about decoding tho? how would the tokenizer decode back to `ت`?
+        # A: The bytes the way they're designed in utf8 sorta gives hint if whether the byte is a sibling to another byte(s).
+        # so if the unkown char is [50,200] then the byte 50 here would somehow give an idea that it should concated with other byte(s) to get a char.
+        self.vocab = {i: bytes([i]) for i in range(256)} # Base vocab. Should be {ID: Bytes}
+        self.pairs = {} # The token merges.  
         pass
 
     # max_vocab is the stopping point. Once the we have max_vocab tokens we stop merging.
     def train(self, dataset, max_vocab):
+        # WORKFLOW: ENCODED DATA > FIND POPULAR PAIR > ADD NEW TOKEN > UPDATE DATASET, REPLACING PAIRS WITH NEW TOKEN > REPEAT TILL STOP POINT. 
         chars = sorted(list(set(dataset)))
-        assert len(chars) < max_vocab, "Dataset has more base tokens than `max_vocab`"
+        # assert len(chars) < max_vocab, "Dataset has more base tokens than `max_vocab`"
+        assert max_vocab > 255, "can't have max_vocab less than length of base vocab." # should be 256?
+        pairing_iter = max_vocab - 256 # could be 0... I guess that's fine
+        data_bytes = dataset.encode('utf-8')
 
+        
+        for i in range(pairing_iter):
+            next_id = i + 256 # base is 0-255. So 1st next token in vocab is 256.
+
+            # constructing a list of the keys alone.
+            # freq_list = list(self._occur_freq(data_bytes)) # There is a better way with `max` (no list construction). forgot how tho.
+            freq_list = self._occur_freq(data_bytes) # There is a better way with `max` (no list construction). forgot how tho.
+            top_pair_key = freq_list[0]
+            self.pairs[top_pair_key] = next_id # wrong?
+
+
+        
         # Vocabulary
-        self.vocab = {char.encode('utf-8'): i for i, char in enumerate(chars)} # Build this?
+        # self.vocab = {char.encode('utf-8'): i for i, char in enumerate(chars)} # Build this?
         
 
         pass
@@ -35,21 +66,29 @@ class Tokenizer:
         pass
 
     # Recieves the byte representation of the text, returns the most occurrent pair?
-    # what if equality?    
-    def _occur_freq(self):
-        pass
+    # CHECK: what if equality?    
+    def _occur_freq(self, data):
+        freqs = dict()
+
+        # When data reaches last el, `zip` wouldn't produce a pair cus `data[1:]` is out of bound.
+        for pair in zip(data, data[1:]):
+            # If key exists, return its value after adding 1, else return 0(fallback) + 1.
+            freqs[pair] = freqs.get(pair, 0) + 1
+        
+        # TODO?: Should just return the max pair? Handle equality of occurrence?
+        freqs_sorted = dict(sorted(freqs.items(), key= lambda item: -item[1]))
+        return freqs_sorted
+
+
+# -------
+# CONFIGS / ARGS
+max_vocab = 300
+# -------
 
 
 with open('taylorswift.txt') as f:
     data = f.read()
 
-chars = sorted(list(set(data)))
-print(chars)
-print(chars[0].encode('utf-8'))
+tokenizer = Tokenizer()
 
-utf_chars = [char.encode('utf-8') for char in chars]
-# utf_chars = {char.encode('utf-8'): i for i, char in enumerate(chars)}
-# print(utf_chars)
-# print([word for word in list(data)[:20]])
-
-print(len(utf_chars))
+tokenizer.train(data, max_vocab)
